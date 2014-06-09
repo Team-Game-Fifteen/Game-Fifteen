02f10c226c988eb547c70f410f5f3309a116db65
ValentinGenev <valentin_genev@abv.bg>
2014-06-10 01:21:11 +0300

    Second commit
    This is the folder that must be refined.

---
+++ TeamProjectGameFifteen-4.git/trunk/GameFifteenRefactored/GameFifteen/HighScore.cs
@@ -0,0 +1,46 @@
+﻿namespace GameFifteen
+{
+    using System;
+
+    class HighScore
+    {
+        private string name;
+        private int score;
+
+        public HighScore(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                // TODO: IMPLEMENT DATA VALIDATION
+                this.name = value;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return this.score;
+            }
+
+            set
+            {
+                //TODO: IMPLEMENT DATA VALIDATION
+                this.score = value;
+            }
+        }
+    }
+}
+
+//test!
